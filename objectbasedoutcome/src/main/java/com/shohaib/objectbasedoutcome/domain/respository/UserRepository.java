3ed1f89a57d994eb036fa113d56c69dac959b60f
MD-Suhad <ssuhad490@gmail.com>
2025-10-04 21:07:10 +0600

    fixed this project file

--- objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/domain/respository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shohaib.objectbasedoutcome.domain.respository;
-
-import com.shohaib.objectbasedoutcome.domain.model.User;
-import jakarta.persistence.Id;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Id> {
-}
