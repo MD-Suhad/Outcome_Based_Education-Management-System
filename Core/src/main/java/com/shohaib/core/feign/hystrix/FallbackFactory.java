0f4fd95cddb8cd2a298d54ec4b658e2dc52e4117
MD-Suhad <ssuhad490@gmail.com>
2025-10-04 20:58:53 +0600

    dependency added

--- Core/src/main/java/com/shohaib/core/feign/hystrix/FallbackFactory.java
+++
@@ -1,7 +0,0 @@
-package com.shohaib.core.feign.hystrix;
-
-import com.shohaib.core.feign.AuthServiceFeignClient;
-
-public interface FallbackFactory<T> {
-    AuthServiceFeignClient create(Throwable throwable);
-}
