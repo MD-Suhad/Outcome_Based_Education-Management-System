ca339b954c3dc66a80b768afb83e3b1c293b999d
MD-Suhad <ssuhad490@gmail.com>
2025-10-11 23:46:41 +0600

    fixed this core issue

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/ObjectbasedoutcomeApplication.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/ObjectbasedoutcomeApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.shohaib.objectbasedoutcome", "com.shohaib.core"})
 public class ObjectbasedoutcomeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ObjectbasedoutcomeApplication.class, args);
 	}
-
 }
