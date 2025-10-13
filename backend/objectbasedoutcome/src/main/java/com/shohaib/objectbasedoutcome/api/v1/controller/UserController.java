537bdfa225a969764a1b167a1081beac226118bc
MD-Suhad <ssuhad490@gmail.com>
2025-10-13 23:03:50 +0600

    fixed all dependency

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/api/v1/controller/UserController.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/api/v1/controller/UserController.java
@@ -10,10 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Validated
+@RequestMapping("/api/v1/auth")
 public class UserController {
 
     @Autowired
