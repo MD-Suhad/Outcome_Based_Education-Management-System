343645fd3199b1f8f0c49855c96aebe75d59d71e
MD-Suhad <ssuhad490@gmail.com>
2025-10-17 22:18:45 +0600

    updated jwt token..

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/util/JWTUtils.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/util/JWTUtils.java
@@ -6,6 +6,11 @@ import com.shohaib.core.util.JWT;
 import lombok.Value;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class JWTUtils {
     @Value("github-secret")
@@ -41,6 +46,40 @@ public class JWTUtils {
             roles = null;
         }
         return roles;
+    }
+
+    public Date getExpirationDate(String token){
+        Date expiration;
+        try{
+            final Claim claims = this.getClaim(token);
+            expiration = claims.getExpiration();
+        }catch(Exception e){
+            expiration = null;
+        }
+        return expiration;
+    }
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String username = getUsername(token);
+        return(username.equals(userDetails.getUsername()) && !isExpire(token));
+    }
+    public ArrayList<String> extractRoles(UserDetails userDetails){
+        ArrayList<String> roles = new ArrayList<>();
+        for(GrantedAuthority ga: userDetails.getAuthorities()){
+            roles.add(ga.getAuthority());
+        }
+        return roles;
+    }
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<String, Object>();
+        claims.put("sub", userDetails.getUsername());
+        claims.put("created", new java.util.Date(System.currentTimeMillis()));
+        claims.put("roles", this.extractRoles(userDetails));
+
+
+        return Jwts.builder().setClaims(claims)
+                .setExpiration(new java.util.Date(System.currentTimeMillis()
+                        + SecurityConstants.EXPIRATION_TIME))
+                .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, this.secret).compact();
     }
 
 
