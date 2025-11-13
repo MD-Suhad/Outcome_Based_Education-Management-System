6149106e8b51d3af5468d83e63baf467177e07f8
MD-Suhad <ssuhad490@gmail.com>
2025-11-13 23:58:58 +0600

    fix this issue

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/configuration/security/JWTAuthenticationFilter.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/configuration/security/JWTAuthenticationFilter.java
@@ -1,8 +1,20 @@
 package com.shohaib.objectbasedoutcome.configuration.security;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class JWTAuthenticationFilter {
+import java.io.IOException;
+
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
     public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
     }
 }
--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/util/JWTUtils.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/util/JWTUtils.java
@@ -1,7 +1,7 @@
 //package com.shohaib.objectbasedoutcome.util;
 //
-////import io.jsonwebtoken.Claims;
-////import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
 //import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.userdetails.UserDetails;
