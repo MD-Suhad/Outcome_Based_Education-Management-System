2f50657104da04a9e9491194d6a11a940ed39436
MD-Suhad <ssuhad490@gmail.com>
2025-11-19 23:17:09 +0600

    FlywayConfiguration

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/configuration/FlywayConfiguration.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/configuration/FlywayConfiguration.java
@@ -1,4 +1,8 @@
 package com.shohaib.objectbasedoutcome.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class FlywayConfiguration {
+    @Autowired
+    private FlywayConfiguration(Da)
 }
