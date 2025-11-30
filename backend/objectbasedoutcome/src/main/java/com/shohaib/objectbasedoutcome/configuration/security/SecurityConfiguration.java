97a534d5f01012b1ca6fe7ae8674f41b6aa9cefc
MD-Suhad <ssuhad490@gmail.com>
2025-11-30 22:36:33 +0600

    updated in project for security

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/configuration/security/SecurityConfiguration.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/configuration/security/SecurityConfiguration.java
@@ -24,6 +24,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
