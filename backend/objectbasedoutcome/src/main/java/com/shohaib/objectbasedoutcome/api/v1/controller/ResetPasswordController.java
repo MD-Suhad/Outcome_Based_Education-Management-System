c9e9f746b667a8660e462dd1b201369b8c9007c4
MD-Suhad <ssuhad490@gmail.com>
2026-04-10 23:07:32 +0600

    updated this file

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/api/v1/controller/ResetPasswordController.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/api/v1/controller/ResetPasswordController.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 @Validated
 public class ResetPasswordController {
     @Autowired
