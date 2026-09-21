443affbc7c5554a9438fb893447724a8f4be4759
MD-Suhad <ssuhad490@gmail.com>
2026-09-21 23:53:48 +0600

    eureka client updated

--- backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/ObjectbasedoutcomeApplication.java
+++ backend/objectbasedoutcome/src/main/java/com/shohaib/objectbasedoutcome/ObjectbasedoutcomeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.shohaib.objectbasedoutcome", "com.shohaib.core"})
 public class ObjectbasedoutcomeApplication {
